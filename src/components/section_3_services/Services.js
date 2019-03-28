756e59de22020a632081b530e8723eea876f04f1
pareen89 <pareenpatel89@gmail.com>
2019-03-27 17:37:05 -0700

    added title to service section

--- src/components/section_3_services/Services.js
+++ src/components/section_3_services/Services.js
@@ -1,11 +1,13 @@
 import React from "react";
 import style from "./services.module.css";
 import Title from "../title/Title";
+import SectionTitle from "./titleContainer/TitleContainer";
 
 const services = () => {
   return (
     <div className={style.background}>
       <Title name="What We Offer" />
+      <SectionTitle />
     </div>
   );
 };
